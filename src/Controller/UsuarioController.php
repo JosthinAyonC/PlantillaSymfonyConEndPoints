cc6f4660cda7ea8e5caeb7b60edbe5e17e89a9f8
JosthinAyon <josthinayon@gmail.com>
2023-04-19 21:29:05 -0500

    get listo

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -15,11 +15,21 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/usuario')]
 class UsuarioController extends AbstractController
 {
-    #[Route('/', name: 'app_usuario_index', methods: ['GET'])]
-    public function index(EntityManagerInterface $entityManager): Response
+    #[Route('/', name: 'app_usuario', methods: ['GET'])]
+    public function indexEditar(EntityManagerInterface $entityManager): Response
     {
         if ($this->isGranted('ROLE_ADMIN')) {
             return $this->render('usuario/index.html.twig');
+        } else {
+            return $this->render('usuario/accesDenied.html.twig');
+        }
+    }
+
+    #[Route('/new', name: 'app_usuario_nuevo', methods: ['GET'])]
+    public function indexNuevo(EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isGranted('ROLE_ADMIN')) {
+            return $this->render('usuario/new.html.twig');
         } else {
             return $this->render('usuario/accesDenied.html.twig');
         }
@@ -39,43 +49,8 @@ class UsuarioController extends AbstractController
         }
     }
 
-    #[Route('/new', name: 'app_usuario_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
-    {
-
-        $usuario = new Usuario();
-        $form = $this->createForm(UsuarioType::class, $usuario);
-        $form->handleRequest($request);
-
-        if ($this->isGranted('ROLE_ADMIN')) {
-            if ($form->isSubmitted() && $form->isValid()) {
-                $usuario->setClave(
-                    $userPasswordHasher->hashPassword(
-                        $usuario,
-                        $form->get('plainPassword')->getData()
-                    )
-                );
-                $roles = $form->get('roles')->getData();
-                $usuario->setRoles([$roles]);
-                $usuario->setEstado("A");
-                $estado = $form->get('estado')->getData();
-                $usuario->setEstado($estado);
-
-                $entityManager->persist($usuario);
-                $entityManager->flush();
-
-                return $this->redirectToRoute('app_usuario_index', [], Response::HTTP_SEE_OTHER);
-            }
-            return $this->renderForm('usuario/new.html.twig', [
-                'usuario' => $usuario,
-                'form' => $form,
-            ]);
-        } else {
-            return $this->render('usuario/accesDenied.html.twig');
-        }
-    }
-
-
+    
+    
     #[Route('/{idUsuario}', name: 'app_usuario_show', methods: ['GET'])]
     public function show(Usuario $usuario): Response
     {
@@ -98,7 +73,7 @@ class UsuarioController extends AbstractController
         ]);
     }
 
-    #[Route('editar/{idUsuario}', name: 'actualizar_usuario', methods: ['PUT'])]
+    #[Route('/{idUsuario}/editar', name: 'actualizar_usuario', methods: ['GET', 'PUT'])]
     public function editarUsuarioPut(Request $request, string $idUsuario, UsuarioRepository $usuarioRepository): Response
     {
 
@@ -114,26 +89,100 @@ class UsuarioController extends AbstractController
         $usuario->setRoles($data['roles']);
         $usuario->setEstado($data['estado']);
         
-
+        
         $usuarioRepository->save($usuario, true);
         
-
+        
         return $this->json($usuario);
     }
 
+    #[Route('/nuevo', name: 'nuevo_usuario', methods: ['POST'])]
+    public function create(
+        Request $request,
+        UsuarioRepository $usuarioRepository,
+    ): Response {
+        
+        $jsonString = $request->getContent();
+        $data = json_decode($jsonString, true);
+        
+        $usuarioNew = new Usuario();
+        
+        $usuarioNew->setNombre($data['nombre']);
+        $usuarioNew->setApellido($data['apellido']);
+        $usuarioNew->setCorreo($data['correo']);
+        $usuarioNew->setClave($data['clave']);
+        $usuarioNew->setRoles($data['roles']);
+        $usuarioNew->setEstado($data['estado']);
 
-    #[Route('/{idUsuario}', name: 'app_usuario_delete', methods: ['POST'])]
-    public function delete(Request $request, Usuario $usuario, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isGranted('ROLE_ADMIN')) {
-            if ($this->isCsrfTokenValid('delete' . $usuario->getIdUsuario(), $request->request->get('_token'))) {
-                $usuario->setEstado("N");
-                $entityManager->flush();
-            }
+        $usuarioRepository->save($usuarioNew, true);
 
-            return $this->redirectToRoute('app_usuario_index', [], Response::HTTP_SEE_OTHER);
-        } else {
-            return $this->render('usuario/accesDenied.html.twig');
-        }
+        return $this->json($usuarioNew);
     }
+    
+    #[Route('/{id}/delete', name: 'delete_usuario', methods: ['PUT'])]
+    public function deleteUsuarios(Request $request, Usuario $usuario, EntityManagerInterface $entity): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $usuario->getIdUsuario(), $request->request->get('_token'))) {
+            $entity->$usuario->setEstado('N');
+            $entity->flush();
+        }
+        
+        
+        return $this->json([
+            'usuario' => $usuario
+        ]);
+    }
+
+    
+    // #[Route('/{idUsuario}', name: 'app_usuario_delete', methods: ['POST'])]
+    // public function delete(Request $request, Usuario $usuario, EntityManagerInterface $entityManager): Response
+    // {
+    //     if ($this->isGranted('ROLE_ADMIN')) {
+        //         if ($this->isCsrfTokenValid('delete' . $usuario->getIdUsuario(), $request->request->get('_token'))) {
+    //             $usuario->setEstado("N");
+    //             $entityManager->flush();
+    //         }
+
+    //         return $this->redirectToRoute('app_usuario_index', [], Response::HTTP_SEE_OTHER);
+    //     } else {
+    //         return $this->render('usuario/accesDenied.html.twig');
+    //     }
+    // }
+
+    // #[Route('/new', name: 'app_usuario_new', methods: ['GET', 'POST'])]
+// public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
+// {
+
+//     $usuario = new Usuario();
+//     $form = $this->createForm(UsuarioType::class, $usuario);
+//     $form->handleRequest($request);
+
+//     if ($this->isGranted('ROLE_ADMIN')) {
+//         if ($form->isSubmitted() && $form->isValid()) {
+//             $usuario->setClave(
+//                 $userPasswordHasher->hashPassword(
+//                     $usuario,
+//                     $form->get('plainPassword')->getData()
+//                 )
+//             );
+//             $roles = $form->get('roles')->getData();
+//             $usuario->setRoles([$roles]);
+//             $usuario->setEstado("A");
+//             $estado = $form->get('estado')->getData();
+//             $usuario->setEstado($estado);
+
+//             $entityManager->persist($usuario);
+//             $entityManager->flush();
+
+//             return $this->redirectToRoute('app_usuario_index', [], Response::HTTP_SEE_OTHER);
+//         }
+//         return $this->renderForm('usuario/new.html.twig', [
+//             'usuario' => $usuario,
+//             'form' => $form,
+//         ]);
+//     } else {
+//         return $this->render('usuario/accesDenied.html.twig');
+//     }
+//     }
 }
+
